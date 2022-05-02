3fffba5f4106b5c9613758dc3acd6e345f9a69a4
Rizki36 <cahrizki.rf@gmail.com>
2022-05-02 18:20:14 +0700

    refactor: add link to logo text

--- src/templates/BaseTemplate/Menus.tsx
+++ src/templates/BaseTemplate/Menus.tsx
@@ -1,22 +1,14 @@
 import { FC } from 'react';
 
-import { HomeIcon, BookOpenIcon } from '@heroicons/react/outline';
+import { BookOpenIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
 const Menus: FC<{ className?: string }> = ({ className }) => {
   return (
     <ul className={`p-2 menu bg-base-100 rounded-box ${className}`}>
       <li>
-        <Link href={'/'}>
-          <a>
-            <HomeIcon className="w-6 h-6" />
-            Landing Page
-          </a>
-        </Link>
-      </li>
-      <li>
         <Link href={'/books'}>
-          <a>
+          <a className="active">
             <BookOpenIcon className="w-6 h-6" />
             Books
           </a>
